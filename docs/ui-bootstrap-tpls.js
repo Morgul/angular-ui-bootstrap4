ae49d8f4df8aa91cea55feb0575b2e711044bbf0
varad_gs <varad.shevade@gslab.com>
2018-07-10 17:34:31 +0530

    carousel-control classname updated in ui-bootstrap-tpls.js from 'left carousel-control' to 'carousel-control-prev' and 'right carousel-control' to 'carousel-control-next'

--- docs/ui-bootstrap-tpls.js
+++ docs/ui-bootstrap-tpls.js
@@ -7491,13 +7491,13 @@ angular.module("uib/template/alert/alert.html", []).run(["$templateCache", funct
 angular.module("uib/template/carousel/carousel.html", []).run(["$templateCache", function ($templateCache) {
   $templateCache.put("uib/template/carousel/carousel.html",
     "<div class=\"carousel-inner\" ng-transclude></div>\n" +
-    "<a role=\"button\" href class=\"left carousel-control\" ng-click=\"prev()\" ng-class=\"{ disabled: isPrevDisabled() }\" ng-show=\"slides.length > 1\">\n" +
+    "<a role=\"button\" href class=\"carousel-control-prev\" ng-click=\"prev()\" ng-class=\"{ disabled: isPrevDisabled() }\" ng-show=\"slides.length > 1\">\n" +
     "  <span aria-hidden=\"true\" class=\"fa-svg-icon\">\n" +
     "    <svg width=\"1792\" height=\"1792\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z\"/></svg>\n" +
     "  </span>\n" +
     "  <span class=\"sr-only\">previous</span>\n" +
     "</a>\n" +
-    "<a role=\"button\" href class=\"right carousel-control\" ng-click=\"next()\" ng-class=\"{ disabled: isNextDisabled() }\" ng-show=\"slides.length > 1\">\n" +
+    "<a role=\"button\" href class=\"carousel-control-next\" ng-click=\"next()\" ng-class=\"{ disabled: isNextDisabled() }\" ng-show=\"slides.length > 1\">\n" +
     "  <span aria-hidden=\"true\" class=\"fa-svg-icon\">\n" +
     "    <svg width=\"1792\" height=\"1792\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z\"/></svg>\n" +
     "  </span>\n" +
